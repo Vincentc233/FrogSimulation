404ee73d4211b1aab7026a55e74427e12c3d0369
Vincentc233 <VIncentc270@nycstudents.net>
2025-05-03 06:54:04 +0000

    FINISHED

--- FrogSimulation.java
+++ FrogSimulation.java
@@ -1,1 +1,50 @@
+public class FrogSimulation
+{
+private int goalDistance;
+private int maxHops;
 
+
+public FrogSimulation(int dist, int numHops)
+{
+    goalDistance = dist;
+    maxHops = numHops;
+}
+
+private int hopDistance()
+{ 
+    int random = ((int)(Math.random() * 61) - 30);
+    return random;
+}
+
+
+public boolean simulate()
+{
+    int distance = 0; 
+    for(int i =0; i<maxHops; i++){
+        distance += hopDistance(); 
+        if(distance >= goalDistance) {
+            System.out.println("Distance:"+ distance);
+            return true;
+        }
+        if(distance < 0) {
+            System.out.println("Distance:"+ distance);
+            return false;
+        }
+    }
+    System.out.println("Distance:"+ distance);
+    return false;
+}
+
+
+public double runSimulations(int num)
+{ 
+    int count  =0;
+    double calculate;
+    for(int i =0; i<num; i++){
+        if(simulate()) count++;
+    }
+    System.out.println(count);
+    calculate = (double) count/num;
+    return calculate;
+}
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args){
+        FrogSimulation frogs = new FrogSimulation(24,5);
+        System.out.println(frogs.simulate());
+        System.out.println(frogs.simulate());
+        System.out.println(frogs.simulate());
+        System.out.println(frogs.simulate());
+        //System.out.println(frogs.runSimulations(100));
+    }
+}
